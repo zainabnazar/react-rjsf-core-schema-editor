3041a82c274123c0ef09aa316005d8ede62a8920
Zainab <zainab.noori@bbc.co.uk>
2024-05-03 13:28:16 +0100

    Test npm i @rjsf/core library for schema editing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Form from '@rjsf/core';
+import validator from '@rjsf/validator-ajv8';
+
+const schema = require('./configs-schema.json')
+
+const uiSchema= {
+    "ui:options": {
+        "semantic": {
+          "fluid": true,
+          "inverted": false,
+          "errorOptions": {
+            "size": "small",
+            "pointing": "above"
+          }
+        }
+      }
+  };
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+        <Form schema={schema} validator={validator} uiSchema={uiSchema}>
+</Form>
+
   );
 }
 
